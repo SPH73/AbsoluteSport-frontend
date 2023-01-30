98ec4bd18f99aea2450e4e1d15e62e4f3b6375a7
Sue Holder <susanpholder@gmail.com>
2023-01-30 22:59:08 +0000

    filter schools on availabilty

--- src/composables/getSchoolList.js
+++ src/composables/getSchoolList.js
@@ -1,40 +1,41 @@
-const Airtable = require('airtable');
+const Airtable = require("airtable");
 const base = new Airtable({ apiKey: process.env.VUE_APP_AT_API_KEY }).base(
-    process.env.VUE_APP_BASE_ID,
+  process.env.VUE_APP_BASE_ID,
 );
 
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const getSchoolList = () => {
-    const table = base('schoolList');
-    const schoolError = ref(null);
-    let school = {};
-    const schoolList = ref([]);
+  const table = base("schoolList");
+  const schoolError = ref(null);
+  let school = {};
+  const schoolList = ref([]);
 
-    const loadSchools = async () => {
-        try {
-            const records = await table
-                .select({
-                    sort: [{ field: 'schoolName', direction: 'asc' }],
-                })
-                .firstPage();
-            if (!records) {
-                throw Error('unable to fetch data');
-            }
-            records.forEach(record => {
-                school = {
-                    id: record.id,
-                    schoolName: record.get('schoolName'),
-                    schoolRef: record.get('schoolRef'),
-                };
-                schoolList.value.push(school);
-            });
-        } catch (err) {
-            schoolError.value = err.message;
-            console.log(schoolError.value);
-        }
-    };
-    return { schoolList, schoolError, loadSchools };
+  const loadSchools = async () => {
+    try {
+      const records = await table
+        .select({
+          filterByFormula: 'NOT(status = "full")',
+          sort: [{ field: "schoolName", direction: "asc" }],
+        })
+        .firstPage();
+      if (!records) {
+        throw Error("unable to fetch data");
+      }
+      records.forEach(record => {
+        school = {
+          id: record.id,
+          schoolName: record.get("schoolName"),
+          schoolRef: record.get("schoolRef"),
+        };
+        schoolList.value.push(school);
+      });
+    } catch (err) {
+      schoolError.value = err.message;
+      console.log(schoolError.value);
+    }
+  };
+  return { schoolList, schoolError, loadSchools };
 };
 
 export default getSchoolList;
